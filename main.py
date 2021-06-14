ef1dcd724a6e2f8353a2581309b657c10e96ae2d
Abdulrehmanghani <amaizingboy197@gmail.com>
2021-06-14 11:34:00 +0500

    main file finshed

--- main.py
+++ main.py
@@ -94,7 +94,14 @@ def connect_mqtt():
 
     return client
 
-
+def preprocessing(frame,net_input_shape):
+    """ 
+    Per-processing on image as required a by network model for input image
+    """
+    image_resize = cv2.resize(frame, (net_input_shape[3], net_input_shape[2]),
+                            interpolation = cv2.INTER_AREA)
+    image = np.moveaxis(image_resize, -1, 0)
+    return image
 
 
 
@@ -114,6 +121,7 @@ def infer_on_stream(args,client):
     prob_threshold = args.prob_threshold
     total = 0
     current = 0
+    last = 0
 
     duration = 0
     in_ROI = [380,450]
@@ -127,7 +135,119 @@ def infer_on_stream(args,client):
     net_input_shape = infer_network.get_input_shape()
     print("Input shape: ", net_input_shape)
 
-   
+    ### TODO: Handle the input stream ###
+    try:
+        cap = cv2.VideoCapture(args.input)
+    except FileNotFoundError:
+        print("Cannot locate video file: "+ args.input)
+    except Exception as e:
+        print("Something else went wrong with the video file: ", e)
+
+    cap.open(args.input)
+    _, frame = cap.read()
+    fps = 0
+    
+    ### TODO: Loop until stream is over ###
+    while cap.isOpened():
+        ### TODO: Read from the video capture ###
+        prev_time = time.time()
+        flag, frame = cap.read()
+        if not flag:
+            break
+
+        ### TODO: Pre-process the image as needed ###
+        image = preprocessing(frame,net_input_shape)
+        ### TODO: Start asynchronous inference for specified request ###
+        infer_network.exec_net(image, request_id=0)
+        inference_time = datetime.timedelta(seconds=time.time() - prev_time)
+        #print("Infrence Time: ",inference_time)
+        #fps = 1/inference_time
+        ### TODO: Wait for the result ###
+        if infer_network.wait(request_id=0) == 0:
+
+            ### TODO: Get the results of the inference request ###
+            result = infer_network.get_output(request_id=0)
+            ### TODO: Extract any desired stats from the results ###
+            for box in result[0][0]: # Output shape is 1x1x100x7
+                conf = box[2]
+                
+                if conf >= prob_threshold:
+                    x1 = int(box[3] * frame.shape[1])
+                    y1 = int(box[4] * frame.shape[0])
+                    x2 = int(box[5] * frame.shape[1])
+                    y2 = int(box[6] * frame.shape[0])
+                    
+                    cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 3)
+                    
+                    
+            ### TODO: Calculate and send relevant information on ###
+            ### current_count, total_count and duration to the MQTT server ###
+            ### Topic "person": keys of "count" and "total" ###
+            ### Topic "person/duration": key of "duration" ###
+            
+                    if x1 < in_ROI[0] and y2 < in_ROI[1]:  
+                        if state == "empty":
+                            # Count a people
+                            current += 1
+                            total += 1
+                            # Start the timer
+                            start_time = time.perf_counter()
+                            # Person entered a room 
+                            state = 'standing'
+                            # Publish people_count messages to the MQTT server
+                           
+                            client.publish("person", json.dumps({"total": total}))
+                                                                 
+
+                    if x1 > out_ROI[0] and y2 < out_ROI[1]:
+                        if state == "standing":
+                            # Change the state to exit - state  change
+                            
+                            stop_time = time.perf_counter()
+                            duration = int(stop_time - start_time)
+                            state = 'empty'
+                            # Update average time
+    
+                            current = 0
+
+                            client.publish("person/duration", json.dumps({"duration": duration}))
+                          
+                    client.publish("person", json.dumps({"count": current}))
+                
+
+            info = [("People in Frame", current) ,             
+                    ("People counter", total),]
+            position = [(10, 50),(10, 85)]  # position at which writing has to start 
+
+            # loop over the info tuples and draw them on our frame
+            for (i, (k, v)) in enumerate(info):
+                text = "{}: {}".format(k, v)                
+                cv2.putText(frame, text,position[i],  
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2)
+            #frmae = cv2.cvtColor(frmae, cv2.COLOR_RGB2BGR)   
+        #     client.publish("person duration", json.dumps({"duration": time_elapsed}))
+        #     client.publish("person", json.dumps({"count": person))
+        # ### TODO: Send the frame to the FFMPEG server ###
+        sys.stdout.buffer.write(frame)
+        
+        sys.stdout.flush()
+        ### TODO: Write an output image if `single_image_mode` ###
+        if args.mod == "img":
+            cv2.imwrite("Image.jpg", img)
+        elif args.mod == "vid":
+            pass
+            #cv2.imshow('frame', frame)
+            #if cv2.waitKey(1) & 0xFF == ord('q'):
+                #break
+            
+    cap.release()
+    client.disconnect()
+    cv2.destroyAllWindows()
+
+    print("FPS: ",fps)
+    print("Infrence Time: ",inference_time)
+    print("Total people count: ",total)
+
 def main():
     """
     Load the network and parse the output.
